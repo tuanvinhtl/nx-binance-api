30d7c3824fac2bd898228b39db806bfb8cce7887
vinh.nguyentuan <tuanvinhtl@gmail.com>
2022-08-20 11:08:38 +0700

    initial commit

---
+++ apps/binance-api-node/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'binance-api-node',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/binance-api-node',
+};
